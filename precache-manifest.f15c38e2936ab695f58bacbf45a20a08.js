4ec47e63f20a2d4d833241af9a762bca2913b85f
Ludovic M <ludo.in.the.sky@gmail.com>
2019-09-22 21:53:55 +0200

    Updates

---
+++ precache-manifest.f15c38e2936ab695f58bacbf45a20a08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b637d0887cbfa990ad26aaad593be376",
+    "url": "/beautifulseconds/index.html"
+  },
+  {
+    "revision": "bf367a55a01b26037b3b",
+    "url": "/beautifulseconds/static/css/main.0df941b7.chunk.css"
+  },
+  {
+    "revision": "b16336f97c5bf80dab79",
+    "url": "/beautifulseconds/static/js/2.1b0b0ab8.chunk.js"
+  },
+  {
+    "revision": "bf367a55a01b26037b3b",
+    "url": "/beautifulseconds/static/js/main.c5b81faf.chunk.js"
+  },
+  {
+    "revision": "3df5a6e73d79ab411003",
+    "url": "/beautifulseconds/static/js/runtime~main.bed996a4.js"
+  }
+]);
